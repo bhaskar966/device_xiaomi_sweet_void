3d8f82e0750ca5f93df8c2c20308a0aa4e149204
bhaskar966 <deybhaskar999@gmail.com>
2022-12-12 23:55:11 +0530

    sweet:Bliss ROM Bringup

--- bliss_sweet.mk
+++ bliss_sweet.mk
@@ -11,14 +11,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit common Voltage OS  Stuff.
-$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
+# Inherit common Bliss OS  Stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 2160
-PRODUCT_NAME := voltage_sweet
+PRODUCT_NAME := bliss_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
-VOLTAGE_BUILD_TYPE := OFFICIAL
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# BlissOS Stuff
+BLISS_BUILDTYPE := UNOFFICIAL
 
