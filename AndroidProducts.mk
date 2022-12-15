62b2299c0fd31711f5dd39efde372b1dd5d68b36
bhaskar966 <deybhaskar999@gmail.com>
2022-12-15 18:40:04 +0530

    Fix: sweet: Awaken OS Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_sweet.mk
+    $(LOCAL_DIR)/awaken_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_sweet-user \
-    evolution_sweet-userdebug \
-    evolution_sweet-eng
+    awaken_sweet-user \
+    awaken_sweet-userdebug \
+    awaken_sweet-eng
--- awaken_sweet.mk
+++ awaken_sweet.mk
@@ -12,14 +12,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
 # Inherit common Evolution  Stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
+PRODUCT_NAME := awaken_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Evolution-X Stuff
-EVO_BUILD_TYPE := UNOFFICIAL
-PRODUCT_NAME := evolution_sweet
+#Awaken OS Stuffs
+WITH_GAPPS := false
+USE_GAPPS := false
+
